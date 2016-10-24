e00c5e742434fb43f70c3993695fd596062228db
mattecologist <matthill@protonmail.com>
2016-10-24 23:38:26 +1100

    updated with some basic data loading

--- model_script.R
+++ model_script.R
@@ -1,10 +1,10 @@
 #### GBIF data
 
 library (rgbif)
+library (dismo)
+library (maptools)
 
-head(name_lookup(query = 'Diuraphis noxia', rank="species", return = 'data'))
-
-## not really enough data at this source.....
+##First, lookup the species on GBIF using the rgbif package
 
 head(name_lookup(query = 'Harmonia axyridis', rank="species", return = 'data'))
 
@@ -17,11 +17,26 @@ key <- name_backbone(name='Harmonia axyridis')$speciesKey
 dat <- occ_search(taxonKey=key, return='data', limit=hold.dat$meta$count)
 gbifmap(dat)
 
-dist <- cbind (dat[,c("species", "decimalLongitude", "decimalLatitude")])
+dist <- cbind (dat[,c("species",  "decimalLatitude", "decimalLongitude")])
 
-### loading raster data
-library (dismo)
-tmin <- getData("worldclim", var = "tmin", res = 10) 
+### loading raster data (example here is the worldclim data)
+### about a 10 mb download at this resolution for the world.
 
+biovar <- getData("worldclim", var = "bio", res = 10) 
 
+##  create a reference raster from this, rescale the points to 1 per grid cell 
+##  and create a new data.frame with this new distribution dataset.
+refrast <- biovar$bio1
+sppR <- rasterize (dist[,3:2], refrast)
+sppP <- data.frame (rasterToPoints (sppR))
 
+### To show now we only have 1 unique point per grid cell:
+length (dist[,1])
+length (sppP[,1])
+
+## load the worldmap from the maptools package
+data("wrld_simpl")
+
+## for the native range we can basically say that region "142" contains the points we are after
+plot (wrld_simpl[wrld_simpl$REGION==142,])
+points (sppP[,1:2], pch=20, col="red")
